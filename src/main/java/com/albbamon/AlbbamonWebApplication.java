a494f1861f5802227bb13c39ad537f9569a6e887
satoshicingu <satoshichingu@gmail.com>
2025-02-28 21:40:21 +0900

    서버 적용 2

--- src/main/java/com/albbamon/AlbbamonWebApplication.java
+++ src/main/java/com/albbamon/AlbbamonWebApplication.java
@@ -5,19 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.albbamon.config.ApiProperties;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class AlbbamonWebApplication {
+public class AlbbamonWebApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AlbbamonWebApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AlbbamonWebApplication.class, args);
-    }
-
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AlbbamonWebApplication.class);
     }
 }
